18084c9635f40fc8fcd7d69cf49c8d6ba2113ec7
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-08-15 01:34:47 +0530

    Auth and OAuth with multi-user feature

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -5,7 +5,6 @@ import {
   DELETE,
   LIKE,
 } from "../constants/actionTypes";
-
 import * as api from "../api/index.js";
 
 export const getPosts = () => async (dispatch) => {
@@ -14,7 +13,7 @@ export const getPosts = () => async (dispatch) => {
 
     dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
@@ -24,7 +23,7 @@ export const createPost = (post) => async (dispatch) => {
 
     dispatch({ type: CREATE, payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
@@ -34,26 +33,28 @@ export const updatePost = (id, post) => async (dispatch) => {
 
     dispatch({ type: UPDATE, payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
 export const likePost = (id) => async (dispatch) => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   try {
-    const { data } = await api.likePost(id);
+    const { data } = await api.likePost(id, user?.token);
 
     dispatch({ type: LIKE, payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    await api.deletePost(id);
+    await await api.deletePost(id);
 
     dispatch({ type: DELETE, payload: id });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRouter);
 
 const PORT = process.env.PORT || 5000;
 
